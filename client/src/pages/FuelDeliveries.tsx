d0864d20dfcc9b670dff6b4c7a974f22438fc0ab
MiriamReichman <104839685+MiriamReichman@users.noreply.github.com>
2023-01-14 22:49:45 +0200

    new Mission

--- client/src/pages/FuelDeliveries.tsx
+++ client/src/pages/FuelDeliveries.tsx
@@ -1,31 +1,59 @@
-import { Container } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, Container, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { AppLayout } from "../layouts/AppLayout";
 import fetchFuledByDate from "../solar-rocket-fuel/getFuelDeliveryByDate";
 import fetchDeliveryDates from "../solar-rocket-fuel/getFuleDeliverieDates";
-import { Dates, DeliveryDate,Error } from "../solar-rocket-fuel/schema";
+import { Dates, DeliveryDate, Error } from "../solar-rocket-fuel/schema";
 
 const FuelDeliveries = (): JSX.Element => {
-  const [fuelDeliveriesDate,setFuelDeliveriesDates] =useState<Dates[] | null>(null);
-  const [fuelDeliveries,setFuelDeliveries] = useState<DeliveryDate|Error|null>(null);
+  const [fuelDeliveriesDate, setFuelDeliveriesDates] = useState<Dates[] | null>(null);
+  const [fuelDeliveries, setFuelDeliveries] = useState<DeliveryDate | Error | null>(null);
   useEffect(() => {
-    fetchDeliveryDates('2023-02-12',4)
-    .then((fule:Dates[] | null)=>setFuelDeliveriesDates(fule));
+    fetchDeliveryDates('2023-02-12', 4)
+      .then((fule: Dates[] | null) => setFuelDeliveriesDates(fule));
   }, [])
   useEffect(() => {
     fetchFuledByDate('2023-02-12')
-    .then((fuleDeliveryDate:DeliveryDate|Error)=>setFuelDeliveries(fuleDeliveryDate));
+      .then((fuleDeliveryDate: DeliveryDate | Error) => setFuelDeliveries(fuleDeliveryDate));
   }, [fuelDeliveriesDate])
   return (
     <AppLayout title="Fuel Deliveries">
       <Container maxWidth="lg">
         <div>Fuel Deliveries!</div>
-       
-         
-                
+
+        {fuelDeliveriesDate ?
+          (
+            <Grid container spacing={2}>
+              {" "}
+              {fuelDeliveriesDate.map((date: Dates, index: number) =>
+              (
+                <Grid item key={index}>
+                  <Card sx={{ width: 275, height: 200 }}>
+                  <CardHeader
+                    title={date}
+                    
+                  />
+                  <CardContent>
+                    <Typography noWrap>{"missions.operator"}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button>Edit</Button>
+                  </CardActions>
+                </Card>
+            </Grid>
+          )
+          )}
+      </Grid>
      
-      </Container>
-    </AppLayout>
+      )
+         :"error"}
+
+
+
+
+
+    </Container>
+    </AppLayout >
   );
 };
 
