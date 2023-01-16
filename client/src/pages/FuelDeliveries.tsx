46add2f5f885f997b17b3392ea4e32f96a4c9f5a
MiriamReichman <104839685+MiriamReichman@users.noreply.github.com>
2023-01-16 17:41:23 +0200

    add view all option

--- client/src/pages/FuelDeliveries.tsx
+++ client/src/pages/FuelDeliveries.tsx
@@ -15,11 +15,15 @@ interface FuelDeliveriesDate {
 
 
 const FuelDeliveries = (): JSX.Element => {
+
   const [date, setDate] = useState<Date>(new Date('2023-02-12'));
   const [numOfDays, setNumberOfDays] = useState<Number>(5)
   const [fuelDeliveriesDate, setFuelDeliveriesDates] = useState<FuelDeliveriesDate | null>(null);
   const [fuelDeliveries, setFuelDeliveries] = useState<DeliveryDate | null>(null);
   const [viewDelevarys, setViewDelevarys] = useState<Date>(new Date('2023-02-12'));
+  const [All, setAll] = useState<boolean>(false);
+  const [viewAllDelevarys, setViewAllDelevarys] = useState<DeliveryDate[]>();
+
 
   useEffect(() => {
     fetchDeliveryDates(date.toISOString().split('T')[0], numOfDays)
@@ -27,27 +31,42 @@ const FuelDeliveries = (): JSX.Element => {
   }, [date, numOfDays])
 
   useEffect(() => {
+    fuelDeliveriesDate?.deliveryDates.forEach((date: String) => {
+      fetchFuledByDate(date)
+        .then((fuleDeliveryDate: DeliveryDate) => {
+          if (viewAllDelevarys) 
+            setViewAllDelevarys([...viewAllDelevarys, fuleDeliveryDate]);
+          else
+            setViewAllDelevarys([fuleDeliveryDate]);
+        })
+    })
+  }, [fuelDeliveriesDate])
+
+  useEffect(() => {
     fetchFuledByDate(viewDelevarys.toISOString().split('T')[0])
       .then((fuleDeliveryDate: DeliveryDate) => {
-
         setFuelDeliveries(fuleDeliveryDate);
       })
   }, [viewDelevarys])
 
-
   const handleClick = (date: string) => {
     setViewDelevarys(new Date(date));
+    setAll(false);
   }
+
+  const handleClickAll = () => {
+    setAll(true);
+  }
+
   return (
     <AppLayout title="Fuel Deliveries">
       <Container maxWidth="lg">
-
         <Typography variant="h4" component="h1">
           Upcoming Fuel Deliveries!
         </Typography>
         <br></br>
         <Box
-          sx={{ maxWidth: 360, bgcolor: 'background.paper', padding: 2, display: 'flex', alignItems: 'flex-end' }}
+          sx={{ maxWidth: 500, bgcolor: 'background.paper', padding: 2, display: 'flex', alignItems: 'flex-end' }}
         >
           <LocalizationProvider dateAdapter={AdapterDateFns}
           >
@@ -62,10 +81,8 @@ const FuelDeliveries = (): JSX.Element => {
               )}
             />
           </LocalizationProvider>
-
-
           <TextField
-            sx={{ minWidth: 200 }}
+            sx={{ minWidth: 200, paddingLeft: 1 }}
             autoFocus
             id="numOfDays"
             label="nummber Of Days to display"
@@ -76,43 +93,39 @@ const FuelDeliveries = (): JSX.Element => {
               var value = parseInt(e.target.value, 10);
               if (value > 5) value = 5;
               if (value < 0) value = 0;
-              setNumberOfDays(value);
-            }}
+              setNumberOfDays(value);}
+            }
           />
         </Box>
-
         <br></br>
         <Typography >
           click on date to view details:
         </Typography>
+
         {fuelDeliveriesDate ?
-        
-
-
-            <Toolbar disableGutters>
-              <Grid justifyContent="flex-end" container>
-                  {
-                    fuelDeliveriesDate.deliveryDates.map((date: string, index: number) => (
-                      <Grid item xs>
-                        <ListItemText key={index} onClick={() => handleClick(date)}>
-                        <ListItemText primary={date} sx={{color:blue}}></ListItemText>
-                          </ListItemText>
-                       </Grid>
-                     
-                  
-                    ))}
-               
-
+          <Toolbar disableGutters>
+            <Grid justifyContent="flex-end" container>
+              <Grid item xs>
+                <ListItem key={"index"}
+                  onClick={() => handleClickAll()}
+                >
+                  <ListItemText primary={"All"} sx={{ color: blue }}></ListItemText>
+                </ListItem>
               </Grid>
-
-
-            </Toolbar>
-          
+              {
+                fuelDeliveriesDate.deliveryDates.map((date: string, index: number) => (
+                  <Grid item xs>
+                    <ListItem key={index + date} onClick={() => handleClick(date)}>
+                      <ListItemText primary={date} sx={{ color: blue }}></ListItemText>
+                    </ListItem>
+                  </Grid>
+                ))
+              }
+            </Grid>
+          </Toolbar>
           : ""}
 
-
-
-        {fuelDeliveries ? (
+        {fuelDeliveries && !All ? (
           <Grid container spacing={2}>
             {" "}
             {fuelDeliveries.deliveries.map((fuelDeliveriesDetials: Delivery, key: number) => (
@@ -120,7 +133,6 @@ const FuelDeliveries = (): JSX.Element => {
                 <Card sx={{ width: 200, height: 300 }}>
                   <CardHeader
                     title={fuelDeliveries.date}
-                  //subheader={new Date(missions.launch.date).toDateString()}
                   />
                   <CardContent>
                     <CardMedia
@@ -128,37 +140,54 @@ const FuelDeliveries = (): JSX.Element => {
                       image={fuelDeliveriesDetials.icon}
                       title="green iguana"
                     />
-                    {/* <Typography noWrap>{fuelDeliveriesDetials.icon}</Typography> */}
-                    {/* <Typography noWrap>{fuelDeliveriesDetials.quantity}</Typography> */}
                     <Typography >type: {fuelDeliveriesDetials.type}</Typography>
                     <Typography >unit :{fuelDeliveriesDetials.unit}</Typography>
                   </CardContent>
-
                 </Card>
               </Grid>
             ))}
           </Grid>
         ) : (
+
           <Box sx={{ textAlign: "center" }}>
-            <CircularProgress />
+            {All ? "" : <CircularProgress />}
           </Box>
         )}
 
+        {viewAllDelevarys && All ? (
+          <Grid container spacing={2}>
+            {viewAllDelevarys.map((fuelDeliveriesDetials: DeliveryDate, index: number) => {
+              let dateTitle = fuelDeliveriesDetials.date;
+              return (
+                <>
+                  {fuelDeliveriesDetials.deliveries.map((deliverie: Delivery, index: number) => (
+                    <Grid item key={index}>
+                      <Card sx={{ width: 200, height: 300 }}>
+                        <CardHeader
+                          title={dateTitle}
+                        />
+                        <CardContent>
+                          <CardMedia
+                            sx={{ height: 160, whidth: 120 }}
+                            image={deliverie.icon}
+                            title="green iguana"
+                          />
+                          <Typography >type: {deliverie.type}</Typography>
+                          <Typography >unit :{deliverie.unit}</Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))}
+                </>
+              )
+            })}
+          </Grid>
+        ) : (
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          <Box sx={{ textAlign: "center" }}>
+            {All ? <CircularProgress /> : ""}
+          </Box>
+        )}
 
       </Container>
     </AppLayout >
